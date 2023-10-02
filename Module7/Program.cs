cd0fad5a327120a2514f17de108869f3aeab83ca
STxSH <anton.batalov94@gmail.com>
2023-10-02 13:26:47 +0700

    task 7.1.4 done

--- Module7/Program.cs
+++ Module7/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+        }
+        
+        //task 7.1.4
+
+        class Employee
+        {
+            public string Name;
+            public int Age;
+            public int Salary;
+        }
+
+        class ProjectManager : Employee
+        {
+            public string projectName;
+        }
+
+        class Developer : Employee
+        {
+            public string programmingLanguage;
         }
     }
 }
